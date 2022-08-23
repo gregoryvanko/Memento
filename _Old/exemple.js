cd9755a9f483bfdb2708798077a967c5a475d3ce
gregoryvanko <gregory.vankoninckxloo@gmail.com>
2022-08-23 22:45:09 +0200

    delete old folder

--- _Old/exemple.js
+++
@@ -1,8 +0,0 @@
-let MementoCoreX = require('./index').MementoCoreX
-const Name = "MementoDev"
-const Port = 5000
-const Debug = true
-//const Icon = __dirname + "/Memento.png"
-//let MyApp = new MementoCoreX(Name, Port, Debug, Icon)
-let MyApp = new MementoCoreX(Name, Port, Debug)
-MyApp.Start()
