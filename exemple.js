5da9b1fbc43a8d07f519e4483daca5a61f743e48
gregoryvanko <gregory.vankoninckxloo@gmail.com>
2025-09-06 13:46:19 +0200

    docker

--- exemple.js
+++ exemple.js
@@ -1,8 +1,8 @@
 const Option = {
     Port:9999,
     Name: process.env.APP_NAME || "Memento",
-    Debug: process.env.APP_DEBUG || false,
-    SplashScreenFilePath: process.env.APP_SCREEN || __dirname + "/Frontend/SplashScreen/SplashScreen.html",
+    Debug: false,
+    SplashScreenFilePath: __dirname + "/Frontend/SplashScreen/SplashScreen.html",
     IconFilePath : process.env.APP_ICON || __dirname + "/Backend/Icon/IconBrown.png",
     MongoDbUrl: "mongodb://mongo:27017"
 }
